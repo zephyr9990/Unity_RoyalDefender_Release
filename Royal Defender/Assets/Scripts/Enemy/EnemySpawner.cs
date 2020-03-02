600b2d1b85b08ddd954efc0c301d19bc9619f3ae
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2020-03-02 15:54:27 -0800

    Deleted - Flying enemy controlled script - EnemyManager
    Created Enemy Spawner script that handles spawning of enemies.

---
+++ Royal Defender/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+    public float spawnTime = 20f;
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length - 1);
+
+        Instantiate(enemyToSpawn, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
+    }
+}
